ae113530f0b6ad571ce7e40a8f7333f31af87f3c
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-04-11 02:55:11 -0400

    Updated portfolio items

--- _lib/mail.php
+++ _lib/mail.php
@@ -1,52 +1,26 @@
-<?php 
-
-    require '../_lib/phpmailer/PHPMailerAutoload.php';
- 
-    // CONFIG YOUR FIELDS
-    //============================================================
-    $name =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $email =    filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $formMessage =  filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-
-    // CONFIG YOUR EMAIL MESSAGE
-    //============================================================
-    $message = '<p>The following request was sent from: </p>';
-    $message .= '<p>Name: ' . $name . '</p>';
-    $message .= '<p>Email: ' . $email . '</p>';
-    $message .= '<p>Message: ' . $formMessage .'</p>';
-
-    // CONFIG YOUR MAIL SERVER
-    //============================================================
-    $mail = new PHPMailer;
-    $mail->isSMTP();                                    // Enable SMTP authentication
-    $mail->SMTPAuth = true;                             // Set mailer to use SMTP
-    //Sign up with MAIL GUN
-    $mail->Host = 'smtp.mailgun.org';                // Specify main and backup server (this is a fake name for the use of this example)             
-
-    $mail->Username = 'postmaster@mycompany.com';                  // SMTP username
-    $mail->Password = '';                         // SMTP password
-    $mail->SMTPSecure = 'tls';                          // Enable encryption, 'ssl' also accepted                                   
-    $mail->Port = 587;                        
-
-    $mail->From = $email;
-    $mail->FromName = $name;
-    $mail->AddReplyTo($email,$name);
-    $mail->addAddress('support@myemail.com', $name);  // Add a recipient
-
-    $mail->WordWrap = 50;                               // Set word wrap to 50 characters
-    $mail->isHTML(true);                                // Set email format to HTML
-
-    $mail->Subject = 'Contact request';
-    $mail->Body    = $message;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    if(!$mail->send()) {
-        $data['error']['title'] = 'Message could not be sent.';
-        $data['error']['details'] = 'Mailer Error: ' . $mail->ErrorInfo;
-       exit;
-    }
-
-    $data['success']['title'] = 'Message has been sent';
-
-    echo json_encode($data);
+<?php
+// Check for empty fields
+if(empty($_POST['first_name'])      ||
+   empty($_POST['last_name'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$first_name = strip_tags(htmlspecialchars($_POST['first_name']));
+$last_name = strip_tags(htmlspecialchars($_POST['last_name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'alfredo.i.urdaneta@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $first_name + $last_name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $first_name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@alfredourdaneta.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
 ?>
