3b2299ce15de22c4994b1390a9931f6ee3f73e26
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-04-11 04:18:10 -0400

    Fixed php mailer

--- _lib/mail.php
+++ _lib/mail.php
@@ -1,26 +1,31 @@
 <?php
-// Check for empty fields
-if(empty($_POST['first_name'])      ||
-   empty($_POST['last_name'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$first_name = strip_tags(htmlspecialchars($_POST['first_name']));
-$last_name = strip_tags(htmlspecialchars($_POST['last_name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'alfredo.i.urdaneta@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $first_name + $last_name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $first_name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@alfredourdaneta.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+require_once('../class.phpmailer.php');
+//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+$name =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+$email =    filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$formMessage =  filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    
+$mail             = new PHPMailer();
+$body             = file_get_contents('contents.html');
+$body             = eregi_replace("[\]",'',$body);
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host       = "smpt.gmail.com"; // SMTP server
+$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+                                           // 1 = errors and messages
+                                           // 2 = messages only
+$mail->SetFrom($email, $name);
+$mail->Subject    = "Conact from your website.";
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+$mail->MsgHTML($body);
+//Recieving email here
+$address = "alfredo.i.urdaneta@gmail.com";
+//Add Your Name
+$mail->AddAddress($address, "Alfredo Urdaneta");
+if(!$mail->Send()) {
+    $data['fail']['title'] = 'Error';
+    echo json_encode($data);
+} else {
+    $data['success']['title'] = 'Message has been sent';
+    echo json_encode($data);
+}
 ?>
