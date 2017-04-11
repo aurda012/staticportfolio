824275e371f02adef8f6496a197957d80f9e854d
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-04-11 18:12:38 -0400

    Changed bg picture, fixed mobile view, and mailer

--- _lib/mail.php
+++ _lib/mail.php
@@ -1,31 +1,42 @@
-<?php
-require_once('../class.phpmailer.php');
-//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-$name =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-$email =    filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-$formMessage =  filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-    
-$mail             = new PHPMailer();
-$body             = file_get_contents('contents.html');
-$body             = eregi_replace("[\]",'',$body);
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "smpt.gmail.com"; // SMTP server
-$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
-                                           // 1 = errors and messages
-                                           // 2 = messages only
-$mail->SetFrom($email, $name);
-$mail->Subject    = "Conact from your website.";
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-$mail->MsgHTML($body);
-//Recieving email here
-$address = "alfredo.i.urdaneta@gmail.com";
-//Add Your Name
-$mail->AddAddress($address, "Alfredo Urdaneta");
-if(!$mail->Send()) {
-    $data['fail']['title'] = 'Error';
-    echo json_encode($data);
-} else {
+<?php 
+    require '../_lib/phpmailer/PHPMailerAutoload.php';
+ 
+    // CONFIG YOUR FIELDS
+    //============================================================
+    $name =     filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email =    filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $formMessage =  filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    // CONFIG YOUR EMAIL MESSAGE
+    //============================================================
+    $message = '<p>The following request was sent from: </p>';
+    $message .= '<p>Name: ' . $name . '</p>';
+    $message .= '<p>Email: ' . $email . '</p>';
+    $message .= '<p>Message: ' . $formMessage .'</p>';
+    // CONFIG YOUR MAIL SERVER
+    //============================================================
+    $mail = new PHPMailer;
+    $mail->isSMTP();                                    // Enable SMTP authentication
+    $mail->SMTPAuth = true;                             // Set mailer to use SMTP
+    //Sign up with MAIL GUN
+    $mail->Host = 'smtp.mailgun.org';                // Specify main and backup server (this is a fake name for the use of this example)             
+    $mail->Username = 'postmaster@mycompany.com';                  // SMTP username
+    $mail->Password = '';                         // SMTP password
+    $mail->SMTPSecure = 'tls';                          // Enable encryption, 'ssl' also accepted                                   
+    $mail->Port = 587;                        
+    $mail->From = $email;
+    $mail->FromName = $name;
+    $mail->AddReplyTo($email,$name);
+    $mail->addAddress('support@myemail.com', $name);  // Add a recipient
+    $mail->WordWrap = 50;                               // Set word wrap to 50 characters
+    $mail->isHTML(true);                                // Set email format to HTML
+    $mail->Subject = 'Contact request';
+    $mail->Body    = $message;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    if(!$mail->send()) {
+        $data['error']['title'] = 'Message could not be sent.';
+        $data['error']['details'] = 'Mailer Error: ' . $mail->ErrorInfo;
+       exit;
+    }
     $data['success']['title'] = 'Message has been sent';
     echo json_encode($data);
-}
 ?>
